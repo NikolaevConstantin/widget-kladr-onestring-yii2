d5127700b71fea7cf1320559131b67caac6ff0e7
pushkin <pushkin@locale.loc>
2018-10-23 16:14:28 +0300

    widget kladr(onestring) for Yii2

---
+++ views/kladr_view.php
@@ -0,0 +1,11 @@
+
+<?php
+use andkon\yii2kladr\assets\KladrAsset;
+
+KladrAsset::register($this); 
+echo '<script src="/js/lib/jquery-1.11.1.min.js"></script>';
+echo '<script src="/js/jquery.kladr.min.js"></script>';
+
+echo '<script src="/js/kladr.js"></script>';
+
+echo "<script>_KLADR.set('{$name}');</script>";
